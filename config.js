9026125f5e4ed5e66d7226e5dc8a44aefde4b8b3
DominicDev <s0l0estudi4r@gmail.com>
2026-02-06 13:48:42 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'NIRVANA';
+export let nameSongs = [
+	"SMELLS LIKE TEEN SPIRIT", /*1*/
+	"COME AS YOU ARE", /*2*/
+	"LITHIUM", /*3*/
+	"IN BLOOM", /*4*/
+	"HEART-SHAPED BOX", /*5*/
+	"ALL APOLOGIES", /*6*/
+	"THE MAN WHO SOLD THE WORLD", /*7*/
+	"WHERE DID YOU SLEEP LAST NIGHT", /*8*/
+	"LAKE OF FIRE", /*9*/
+	"SOMETHING IN THE WAY", /*10*/
+	"BREED", /*11*/
+	"DRAIN YOU", /*12*/
+	"ANEURYSM", /*13*/
+	"DIVE", /*14*/
+	"BIG LONG NOW", /*15*/
+	"SIFTING", /*16*/
+	"SWAP MEET", /*17*/
+	"MR. MOUSTACHE", /*18*/
+	"AERO ZEPPELIN", /*19*/
+	"HAIRSPRAY QUEEN", /*20*/
+]
